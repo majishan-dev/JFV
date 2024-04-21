b7dcd0428697c458b81b04cccd6c8346738e429e
hiropay <hiropay28@gmail.com>
2024-04-21 17:20:43 +0900

    グラフ2パターン作成

--- JFV_app/urls.py
+++ JFV_app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views.h_views import show_top
+from .views.h_views import show_top, show_test
 
 from .views import finance_chart
 
 urlpatterns = [
     path('top', show_top),
+    path('test', show_test),
     path('chart', finance_chart),
 ]
--- JFV_app/views/h_views.py
+++ JFV_app/views/h_views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def show_test(request):
+  return render(request, 'test.html')
+
 def show_top(request):
   return render(request, 'top.html')
 
